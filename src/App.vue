831b203884f1fb286e629f1ad727b054088bb01f
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-12-14 11:08:24 -0600

    fix

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@ Draggable.create(".knob", {
           when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
           It has survived not only five centuries.</p>
       </div>
-      <div class="container col-start-1 row-start-1 z-[99] md:order-1 h-[150dvh] md:h-fit w-full md:static md:w-[40vw] md:min-w-[350px] md:max-w-[450px] md:px-2 md:top-0 md:flex justify-center items-center">
+      <div class="container col-start-1 row-start-1 z-[99] md:order-1 h-[140dvh] md:h-fit w-full md:static md:w-[40vw] md:min-w-[350px] md:max-w-[450px] md:px-2 md:top-0 md:flex justify-center items-center">
         <div id="pedal-div" class="w-full h-fit md:w-[350px] min-h-[100dvh] px-6 bg-ben-white/30 backdrop-blur md:backdrop-blur-none rounded-t-[50px] pt-4 md:pt-0 md:bg-transparent">
           <div id="pedal" class="max-h-[60dvh] w-[80%] m-auto">
             <Pedal/>
