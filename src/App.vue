ea3fa680dc92bb0a838e258770ae8036897973d4
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-12-14 10:43:17 -0600

    fix

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,7 @@ Draggable.create(".knob", {
           It has survived not only five centuries, but also the leap into electronic typesetting, 
           remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem </p>
       </div>
-      <div class="container col-start-1 row-start-1 z-[99] md:order-1 h-[160dvh] md:h-fit w-full md:static md:w-[40vw] md:min-w-[350px] md:max-w-[450px] md:px-2 md:top-0 md:flex justify-center items-center">
+      <div class="container col-start-1 row-start-1 z-[99] md:order-1 h-[170dvh] md:h-fit w-full md:static md:w-[40vw] md:min-w-[350px] md:max-w-[450px] md:px-2 md:top-0 md:flex justify-center items-center">
         <div id="pedal-div" class="w-full h-fit md:w-[350px] min-h-[100dvh] px-6 bg-ben-white/30 backdrop-blur md:backdrop-blur-none rounded-[50px] pt-4 md:pt-0 md:bg-transparent">
           <div id="pedal" class="max-h-[60dvh] w-[80%] m-auto">
             <Pedal/>
